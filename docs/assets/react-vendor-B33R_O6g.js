a777faf5bb3e3a8a0bf119f6252710ddadf84a04
Arish limbu <limbuarish0@gmail.com>
2026-08-28 10:17:15 +0545

    Configure deployment to use docs folder and remove floating shapes

---
+++ docs/assets/react-vendor-B33R_O6g.js
@@ -0,0 +1,1 @@
+import"./three-vendor-DHB7GFL4.js";import"./animation-vendor-D7XEHKlI.js";
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/arish-limbu-portfolio/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
     sourcemap: false,
     rollupOptions: {
